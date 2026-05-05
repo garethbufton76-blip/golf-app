cc210cd0aae85c5b19ca4e6613082482f563b8e8
garethbufton76-blip <garethbufton76@hotmail.com>
2026-05-05 20:54:59 +0100

    Create App.tsx

---
+++ public/src/App.tsx
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+import Home from "./components/Home";
+import Score from "./components/Score";
+
+export default function App() {
+  const [screen, setScreen] = useState<"home" | "score">("home");
+
+  return (
+    <div className="min-h-[100svh] w-full bg-black text-white">
+      {screen === "home" && <Home setScreen={setScreen} />}
+      {screen === "score" && <Score setScreen={setScreen} />}
+    </div>
+  );
+}
